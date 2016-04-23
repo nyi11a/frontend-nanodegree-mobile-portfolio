7a6f2c8ee5239fc6afbe01fbcfb2e1d192e41081
N Yilla <ms.yilla@gmail.com>
2016-04-23 23:51:59 +0400

    Create dist folder and accompanying gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'views/js/main.js',
+        dest: 'dist/views/js/main.min.js',
+      }
+    },
+    cssmin: {
+        minify: {
+        src: 'views/css/style.css',
+        dest: 'dist/views/css/style.min.css'
+      }
+    },
+    htmlmin: {
+        minify: {
+        src: 'views/pizza.html',
+        dest: 'dist/views/pizza.min.html'
+      }
+    },
+    processhtml: {
+        build: {
+            files: {
+                'dist/views/js/main.min.js' : ['views/js/main.js'],
+                'dist/views/css/style.min.css' : ['views/css/style.css']
+            }
+        }
+    }
+});
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-processhtml');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','cssmin', 'htmlmin']);
+
+};
